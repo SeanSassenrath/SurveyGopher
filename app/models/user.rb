b550f561c49268e5e9c86e1f0337e7b776d49f56
What did the fox say? & Sean Sassenrath <pair+nguyennam9696.seansassenrath@devbootcamp.com>
2015-05-08 11:29:16 -0700

    stupid f commit

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-
-  has_many :my_surveys, class_name: 'Survey', foreign_key: 'creator_id'
-  has_many :answers
-  has_many :taken_surveys, class_name: 'Survey', through: :answers, source: :survey
-end
